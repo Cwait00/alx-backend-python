6a5143ab53ae64fb6aae0325f749c3a985c5abd2
Cwait00 <cwaitanobongoza@gmail.com>
2024-05-03 15:11:38 +0000

    8-make_multiplier.py

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
 
+
+"""
+This module provides a function to create a multiplier function.
+"""
+
+
 from typing import Callable
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
