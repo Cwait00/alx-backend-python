f65f121821dc96a3451690918ed290f83e073b94
Cwait00 <cwaitanobongoza@gmail.com>
2024-05-03 15:52:28 +0000

    102-type_checking.py

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python3
 
+
+"""
+This script defines a function to zoom in on the elements of a tuple by repeating each element 'factor' times.
+"""
+
 from typing import Tuple, List
 
-def zoom_array(lst: Tuple, factor: int = 2) -> List[int]:
+
+def zoom_array(lst: Tuple[int], factor: int = 2) -> List[int]:
+    """
+    Zooms in on the elements of a tuple by repeating each element 'factor' times.
+
+    Args:
+        lst (Tuple[int]): The input tuple.
+        factor (int, optional): The factor by which to zoom in. Defaults to 2.
+
+    Returns:
+        List[int]: The zoomed-in list.
+    """
     zoomed_in: List[int] = [
         item for item in lst
         for i in range(factor)
