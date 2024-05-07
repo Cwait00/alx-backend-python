f1f931cca0dc4dce42e9565d5a5d6d3df468be4f
Cwait00 <cwaitanobongoza@gmail.com>
2024-05-07 19:06:18 +0000

    1-async_comprehension.py

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+1-async_comprehension module
+"""
+
+import asyncio
+from typing import List
+import random
+
+async def async_comprehension() -> List[float]:
+    """
+    Collect 10 random numbers using an async comprehension.
+
+    Returns:
+        List[float]: A list of 10 random numbers.
+    """
+    return [random.uniform(0, 10) for _ in range(10)]
+
