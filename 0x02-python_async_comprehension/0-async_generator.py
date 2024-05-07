f937c9c103e4b514deb92cdcbcf1bb079b86add4
Cwait00 <cwaitanobongoza@gmail.com>
2024-05-07 14:57:02 +0000

    0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,13 +4,12 @@
 import asyncio
 import random
 
-
 async def async_generator():
     """
     This coroutine generates a sequence of random numbers.
 
-    It asynchronously waits 1 second between each number and yields a
-    random number between 0 and 10.
+    It asynchronously waits 1 second between each number and yields
+    a random number between 0 and 10.
     """
     random.seed(42)  # Set a fixed seed for reproducibility
     for _ in range(10):
