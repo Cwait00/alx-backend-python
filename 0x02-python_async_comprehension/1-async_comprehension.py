2edc96d62df0e6cbdbf2a59c487e16eb27d613be
Cwait00 <cwaitanobongoza@gmail.com>
2024-05-10 18:38:02 +0000

    updated-1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -19,7 +19,7 @@ async def async_comprehension() -> List[float]:
         List[float]: A list of 10 random numbers.
     """
 
-    return [await asyncio.sleep(0, random.uniform(0, 10)) async for _ in range(10)]
+    return [await asyncio.sleep(0, random.uniform(0, 10)) for _ in range(10)]
 
 
 # Add two blank lines at the end of the file # noqa
