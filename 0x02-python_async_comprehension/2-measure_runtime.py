5d57605ddafc4b41593be2a874c5bb488c2c7033
Cwait00 <cwaitanobongoza@gmail.com>
2024-05-10 21:10:22 +0000

    -a

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,77 +1,40 @@
 #!/usr/bin/env python3
 
+
 """
 2-measure_runtime module
 """
 
 
 import asyncio
-import time
+from typing import List
 
-from typing import List, AsyncIterator
-
-
-async def async_generator(n: int) -> AsyncIterator[int]:
-    """
-    Generate an asynchronous generator that yields numbers from 0 to n-1.
-    """
-
-    for i in range(n):
-        yield i
-
-
-async def async_comprehension() -> List[int]:
-    """
-    Collect 10 random numbers using an async comprehension.
-    """
-
-    return [number async for number in async_generator(10)]
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
     """
-    Measure the runtime of executing async_comprehension four
-    times in parallel.
-    """
+    Measure the total runtime of executing async_comprehension
+    four times in parallel.
 
-    start_time = time.monotonic()
+    Returns:
+        float: Total runtime.
+    """
+    start_time = asyncio.get_event_loop().time()  # Record start time
+
+    # Execute async_comprehension four times in parallel using asyncio.gather
     await asyncio.gather(
         async_comprehension(),
         async_comprehension(),
         async_comprehension(),
         async_comprehension()
     )
-    end_time = time.monotonic()
-    total_runtime = end_time - start_time
+
+    end_time = asyncio.get_event_loop().time()  # Record end time
+
+    total_runtime = end_time - start_time  # Calculate total runtime
     return total_runtime
 
 
 if __name__ == "__main__":
-    runtime = asyncio.run(measure_runtime())
-
-    if runtime <= 11:
-        print(
-            "Function returns a runtime that is within 10 seconds plus "
-            "a 10% overhead"
-        )
-
-    print(
-        asyncio.run(measure_runtime()) == asyncio.gather(
-            async_comprehension(),
-            async_comprehension(),
-            async_comprehension(),
-            async_comprehension()
-        )
-    )
-
-    print(
-        isinstance(runtime, float)
-    )
-
-    print(
-        all(func.__doc__ for func in (
-            async_generator,
-            async_comprehension,
-            measure_runtime
-        ))
-    )
+    asyncio.run(measure_runtime())
