e897f05d652277b0be95cff847303309e6b413b4
Cwait00 <cwaitanobongoza@gmail.com>
2024-05-10 20:54:44 +0000

    -a

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -4,15 +4,18 @@
 2-measure_runtime module
 """
 
-import asyncio
 
-from typing import List, AsyncIterator  # Import AsyncIterator
+import asyncio
+import time
+
+from typing import List, AsyncIterator
 
 
 async def async_generator(n: int) -> AsyncIterator[int]:
     """
     Generate an asynchronous generator that yields numbers from 0 to n-1.
     """
+
     for i in range(n):
         yield i
 
@@ -21,28 +24,54 @@ async def async_comprehension() -> List[int]:
     """
     Collect 10 random numbers using an async comprehension.
     """
+
     return [number async for number in async_generator(10)]
 
 
 async def measure_runtime() -> float:
     """
-    Measure the runtime of executing async_comprehension four times in parallel.
+    Measure the runtime of executing async_comprehension four
+    times in parallel.
     """
-    # Execute async_comprehension four times in parallel
-    start_time = asyncio.get_event_loop().time()
+
+    start_time = time.monotonic()
     await asyncio.gather(
         async_comprehension(),
         async_comprehension(),
         async_comprehension(),
         async_comprehension()
     )
-    end_time = asyncio.get_event_loop().time()
-
-    # Calculate the total runtime
+    end_time = time.monotonic()
     total_runtime = end_time - start_time
-
     return total_runtime
 
 
 if __name__ == "__main__":
-    asyncio.run(measure_runtime())
+    runtime = asyncio.run(measure_runtime())
+
+    if runtime <= 11:
+        print(
+            "Function returns a runtime that is within 10 seconds plus "
+            "a 10% overhead"
+        )
+
+    print(
+        asyncio.run(measure_runtime()) == asyncio.gather(
+            async_comprehension(),
+            async_comprehension(),
+            async_comprehension(),
+            async_comprehension()
+        )
+    )
+
+    print(
+        isinstance(runtime, float)
+    )
+
+    print(
+        all(func.__doc__ for func in (
+            async_generator,
+            async_comprehension,
+            measure_runtime
+        ))
+    )
