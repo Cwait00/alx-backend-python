0fe0b88b3898b3d8156745706db07925396fc576
cwait00 <cwaitanobongoza@gmail.com>
2024-07-22 15:24:20 +0000

    Fix async comprehension to use async for loop in list comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,14 +1,24 @@
 #!/usr/bin/env python3
 
-
 """
 1-async_comprehension module
 """
 
-
 import asyncio
 from typing import List
 import random
+
+
+async def generate_random_numbers() -> List[float]:
+    """
+    Generate 10 random numbers asynchronously.
+
+    Yields:
+        float: A random float between 0 and 10.
+    """
+    for _ in range(10):
+        await asyncio.sleep(0)
+        yield random.uniform(0, 10)
 
 
 async def async_comprehension() -> List[float]:
@@ -18,8 +28,6 @@ async def async_comprehension() -> List[float]:
     Returns:
         List[float]: A list of 10 random numbers.
     """
-
-    return [await asyncio.sleep(0, random.uniform(0, 10)) for _ in range(10)]
-
+    return [number async for number in generate_random_numbers()]
 
 # Add two blank lines at the end of the file # noqa
